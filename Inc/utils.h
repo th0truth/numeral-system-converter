080a140b38794c5db59b08c0760e8e6c1ef996bf
th0truth <shiftrams@gmail.com>
2026-01-07 12:05:17 +0200

    refactor: not minor stylish

--- Inc/utils.h
+++ Inc/utils.h
@@ -7,7 +7,7 @@
 #define BUFFER_SIZE 128
 #define FORMAT_SIZE 8
 
-int extractNumberFormat(const char *buffer, char *number, char *format);
-int converter(char *res, const char *number, const char *format);
+int extractNumberFormat(const char* buffer, char* number, char* format);
+int converter(char* res, const char* number, const char* format);
 
 #endif
