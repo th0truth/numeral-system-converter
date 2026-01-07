5ed8bcbbbf1ee15cdcd5822a0f254bc7b6292857
th0truth <shiftrams@gmail.com>
2026-01-07 11:09:45 +0200

    feat: add header files

---
+++ Inc/ns.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifndef NS_H
+#define NS_H
+
+int checkBin(const char* bin);
+long binToDec(const char* bin);
+long binToOctal(const char* bin);
+
+long hexToDec(const char* hex);
+
+#endif
